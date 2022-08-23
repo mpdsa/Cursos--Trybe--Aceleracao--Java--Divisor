c3dd1a7ebb2dfedebb2b5bd5a8a924dc1ecd813e
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-08-23 16:37:00 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/Divisor.java
+++ src/main/java/com/trybe/acc/java/Divisor.java
@@ -1,5 +1,16 @@
 package com.trybe.acc.java;
 
+import java.lang.ArithmeticException;
+
+/** Classe divisor. */
 public class Divisor {
 
+  /** Método dividir. */
+  public int dividir(int a, int b) {
+    try {
+      return a / b;
+    } catch (ArithmeticException e) {
+      return 0;
+    }
+  }
 }
